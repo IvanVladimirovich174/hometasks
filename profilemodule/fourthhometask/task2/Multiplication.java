a7dff29fb36eff696dd42c448c01c8b6b06a9264
IvanVladimirovich174 <50528755+IvanVladimirovich174@users.noreply.github.com>
2022-11-30 03:02:38 +0300

    Hometasks

--- profilemodule/fourthhometask/task2/Multiplication.java
+++ profilemodule/fourthhometask/task2/Multiplication.java
@@ -5,6 +5,6 @@ import java.util.List;
 public class Multiplication {
     public static int getMultiplicationOfListValues(List<Integer> list) {
         return list.stream()
-                .reduce(1,(firstNumber, secondNumber) -> firstNumber * secondNumber);
+                .reduce(1, (firstNumber, secondNumber) -> firstNumber * secondNumber);
     }
 }
