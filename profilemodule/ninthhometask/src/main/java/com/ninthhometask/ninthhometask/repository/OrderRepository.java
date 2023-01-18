ed93654a9590449cc0082978c4b66de9e8eb3817
SOLIDMAN <ivanvladimirovich174@yandex.ru>
2023-01-19 02:12:04 +0500

    Hometasks

--- profilemodule/ninthhometask/src/main/java/com/ninthhometask/ninthhometask/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ninthhometask.ninthhometask.repository;
-
-import com.ninthhometask.ninthhometask.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- profilemodule/ninthhometask/src/main/java/com/ninthhometask/ninthhometask/repository/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ninthhometask.ninthhometask.repository;
-
-import com.ninthhometask.ninthhometask.model.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-}
